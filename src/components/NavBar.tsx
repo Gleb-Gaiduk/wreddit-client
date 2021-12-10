3abf94e0a0a0f0c5930bb627f7905ad74e04e624
Hleb Haiduk <glebgaiduk.pl@gmail.com>
2021-12-10 14:39:58 +0100

    Implement auth me / login feature

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,29 @@
-import { Box, Flex, Link } from '@chakra-ui/react';
+import { Box, Button, Flex, Link } from '@chakra-ui/react';
 import NextLink from 'next/link';
-import React from 'react';
+import React, { ReactNode } from 'react';
+import { useMeQuery } from '../generated/graphql';
 
 type TNavBarProps = {};
 
 export const NavBar = ({}: TNavBarProps) => {
-  return (
-    <Flex bg='tomato' p={4}>
-      <Box ml={'auto'}>
+  const [{ fetching, data }] = useMeQuery();
+
+  let body: ReactNode | null = null;
+
+  if (fetching) {
+    // Do smth while fetching
+  } else if (data?.auth) {
+    // User is logged in
+    body = (
+      <Flex>
+        <Box mr={4}>{data.auth.username}</Box>
+        <Button variant='link'>Logout</Button>
+      </Flex>
+    );
+  } else {
+    // User is not logged in
+    body = (
+      <>
         <NextLink href='/login'>
           <Link mr={2} color={'white'}>
             Login
@@ -17,7 +33,13 @@ export const NavBar = ({}: TNavBarProps) => {
         <NextLink href='/register'>
           <Link color={'white'}>Register</Link>
         </NextLink>
-      </Box>
+      </>
+    );
+  }
+
+  return (
+    <Flex bg='tomato' p={4}>
+      <Box ml={'auto'}>{body}</Box>
     </Flex>
   );
 };
