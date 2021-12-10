24b329e9506cd2bdf6be1507227a074ebd63a0d8
Hleb Haiduk <glebgaiduk.pl@gmail.com>
2021-12-10 10:28:24 +0100

    Set up basic graphql client

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,16 +1,34 @@
 import { Button } from '@chakra-ui/react';
 import { Form, Formik } from 'formik';
+import { useMutation } from 'urql';
 import { InputField } from '../components/InputField';
 import { EVariant, Wrapper } from '../components/Wrapper';
 
 type TRegisterProps = {};
 
+const REGISTER_MUTATION = `
+  mutation Register($username: String!, $password: String!) {
+    register(options: { username: $username, password: $password}) {
+      errors {
+        field
+        message
+      }
+      user {
+        id
+        username
+      }
+    }
+  }
+`;
+
 const Register = ({}: TRegisterProps) => {
+  const [{}, register] = useMutation(REGISTER_MUTATION);
+
   return (
     <Wrapper variant={EVariant.SMALL}>
       <Formik
         initialValues={{ username: '', password: '' }}
-        onSubmit={values => console.log(values)}
+        onSubmit={values => register(values)}
       >
         {({ values, handleChange, isSubmitting }) => (
           <Form>
@@ -27,7 +45,7 @@ const Register = ({}: TRegisterProps) => {
             <Button
               type='submit'
               mt={4}
-              variantColor='teal'
+              colorScheme='teal'
               isLoading={isSubmitting}
             >
               Register
