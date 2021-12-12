e9618ae790e8e00116fd7ad5e60ea2b1e7edd6be
Hleb Haiduk <glebgaiduk.pl@gmail.com>
2021-12-12 10:11:00 +0100

    Make index page SSR-rendered

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
 import { withUrqlClient } from 'next-urql';
 import { NavBar } from '../components/NavBar';
+import { usePostsQuery } from '../generated/graphql';
 import { createUrqlClient } from '../utils/createUrqlClient';
 
-const Index = () => (
-  <>
-    <NavBar />
-    <div>Hello World</div>
-  </>
-);
+const Index = () => {
+  const [{ fetching, data }] = usePostsQuery();
+  // Finished on 3.58
+  return (
+    <>
+      <NavBar />
+      <div>Hello World</div>
+      <br />
+      {data ? (
+        data.posts.map(post => <div key={post.id}>{post.title}</div>)
+      ) : (
+        <div>Loading...</div>
+      )}
+    </>
+  );
+};
 
-export default withUrqlClient(createUrqlClient)(Index);
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
